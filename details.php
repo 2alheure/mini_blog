156e7ceceef3621f1cd1f96f1904e75c0268ffd2
2alheure <jojo77.juv@live.fr>
2023-02-28 10:16:51 +0100

    Fonctionnalité détails fonctionnelle

--- details.php
+++ details.php
@@ -3,5 +3,7 @@
 require_once './Page.php';
 $page = new Page();
 
-
-$page->sAfficher('details.html.twig');
+$clef = $_GET['clef'];
+$page->sAfficher('details.html.twig', [
+    'article' => $_SESSION['articles'][$clef],
+]);
