9da31aa83931760bd654b3b35808f5586736cdc5
2alheure <jojo77.juv@live.fr>
2023-02-28 16:42:31 +0100

    Poster commentaire OK

---
+++ poster_commentaire.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once __DIR__ . '/Page.php';
+$page = new Page();
+
+$clef = $_GET['clef'];
+
+$nouveauCommentaire = $_POST;
+$nouveauCommentaire['date'] = date('d/m/Y H:i');
+$_SESSION['articles'][$clef]['commentaires'][] = $nouveauCommentaire;
+
+
+
+$page->rediriger('details.php?clef=' . $clef);
